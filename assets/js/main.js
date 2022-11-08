79f5b4aee480ef8232945e51cbe58ad8bc915556
payam rabiei <rabiei.p@gmail.com>
2022-11-07 20:42:35 -0800

    fix the jitter in form refresher

--- assets/js/main.js
+++ assets/js/main.js
@@ -166,25 +166,25 @@ document.addEventListener('DOMContentLoaded', () => {
             gap[0] = input[1].getBoundingClientRect().left-input[0].getBoundingClientRect().left;
             gap[1] = input[2].getBoundingClientRect().left-input[1].getBoundingClientRect().left;
             
-            input[0].style.transitionDuration = '1s';
+            input[0].style.transitionDuration = '0.5s';
             input[0].style.transform = ["translateX(" + gap[0].toString() + "px)"];
             input[0].style.opacity = 1;
             input[0].style.width = '40%';
             input[0].addEventListener('transitionend', () => {
                 //Reset
-                input[0].style.transitionDuration = '0.001s';
+                input[0].style.transitionDuration = '0.0s';
                 input[0].style.transform = 'translateX(0px)';
                 input[0].style.opacity = 0;
                 input[0].style.width = '0%';
                 
             });
-            input[1].style.transitionDuration = '1s';
+            input[1].style.transitionDuration = '0.5s';
             input[1].style.transform = ["translateX(" + gap[1].toString() + "px)"];
             input[1].style.opacity = 0;
             input[1].style.width = '0%';
             input[1].addEventListener('transitionend', () => {
                 //Reset
-                input[1].style.transitionDuration = '0.001s';
+                input[1].style.transitionDuration = '0.0s';
                 input[1].style.transform = 'translateX(0px)';
                 input[1].style.opacity = 1;
                 input[1].style.width = '40%';
@@ -203,24 +203,24 @@ document.addEventListener('DOMContentLoaded', () => {
             let gap = [];
             gap[0] = input[1].getBoundingClientRect().right-input[2].getBoundingClientRect().right;
             gap[1] = input[0].getBoundingClientRect().right-input[1].getBoundingClientRect().right;
-            input[2].style.transitionDuration = '1s';
+            input[2].style.transitionDuration = '0.5s';
             input[2].style.transform = ["translateX(" + gap[0].toString() + "px)"];
             input[2].style.opacity = 1;
             input[2].style.width = '40%';
             input[2].addEventListener('transitionend', () => {
                 //Reset
-                input[2].style.transitionDuration = '0.001s';
+                input[2].style.transitionDuration = '0.0s';
                 input[2].style.transform = 'translateX(0px)';
                 input[2].style.opacity = 0;
                 input[2].style.width = '0%';
             });
-            input[1].style.transitionDuration = '1s';
+            input[1].style.transitionDuration = '0.5s';
             input[1].style.transform = ["translateX(" + gap[1].toString() + "px)"];
             input[1].style.opacity = 0;
             input[1].style.width = '0%';
             input[1].addEventListener('transitionend', () => {
                 //Reset
-                input[1].style.transitionDuration = '0.001s';
+                input[1].style.transitionDuration = '0.0s';
                 input[1].style.transform = 'translateX(0px)';
                 input[1].style.opacity = 1;
                 input[1].style.width = '40%';
