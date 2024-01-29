44eafd1c1d877815073a81f89e7b71bea94b4b59
payam rabiei <rabiei.p@gmail.com>
2024-01-29 12:51:47 +0700

    strip payment

--- assets/php/paymentGateway.php
+++ assets/php/paymentGateway.php
@@ -7,7 +7,7 @@ require '../../vendor/autoload.php';
 function processPayment($data) {
 
     $stripe = new Stripe\StripeClient('sk_test_51Odb1JGvkwgMtml80Bc0CdBOesMqZzMeulH9j8QO03HnfrLniWn96gEYLK9QdLbmmXQ1voYVKBib06UaTdqxgzfP00P41SGnWu');
-    $customerObj = $stripe->customers->create([
+    $stripe->customers->create([
         'description' => 'example customer',
         'email' => 'email@example.com',
         'payment_method' => 'pm_card_visa',
@@ -16,20 +16,20 @@ function processPayment($data) {
         'amount' => 50,
         'currency' => 'usd',
         'automatic_payment_methods' => ['enabled' => true],
-      ]);
-    $paymentIntentConf = $stripe->paymentIntents->confirm(
+    ]);
+    $stripe->paymentIntents->confirm(
         $paymentIntent->id,
         [
           'payment_method' => 'pm_card_visa',
           'return_url' => 'https://www.example.com',
         ]
-      );
-    $chargeObj = $stripe->charges->create([
+    );
+    $stripe->charges->create([
         'amount' => 50,
         'currency' => 'usd',
         'source' => 'tok_visa',
     ]);
-    echo $chargeObj;
+    
 }
 
 
